a160273158b54021543876eb5e5d7d432b2e92e2
claudiasellers <50253395+claudiasellers@users.noreply.github.com>
2021-03-04 15:00:57 -0700

    Update mixpanel.js

--- musicfinder/js/mixpanel.js
+++ musicfinder/js/mixpanel.js
@@ -8,8 +8,8 @@ function signupPageViewed() {
 // The "user" object contains the following properties: name, email, favorite_genre, plan, id
 // e.g. calling user.id will return the user's id
 function accountCreated(user) {
-	mixpanel.track("Account Created", {"Name":name, "Email":email, "Genre": favorite_genre})
-	mixpanel.identify(name)
+	mixpanel.track("Account Created", {"Name":user.name, "Email":user.email, "Genre": user.favorite_genre})
+	mixpanel.identify(user.name)
 	
 }
 
@@ -17,25 +17,27 @@ function accountCreated(user) {
 // The "user" object contains the following properties: name, email, favorite_genre, plan, id
 // e.g. calling user.id will return the user's id
 function login(user) {
-	mixpanel.track("Login")
+	mixpanel.track("Login", {"Name":user.name, "Email":user.email, "Genre": user.favorite_genre, "Plan": user.plan})
 }
 
 // This function executes every time a song is played
 // The "song" object contains the following properties: title, artist, genre, duration
 // e.g. calling song.title will return the song's title
 function songPlayed(song) {
-	mixpanel.track("Song Played")
+	mixpanel.track("Song Played", {"Title": song.title, "Artist": song.artist, "Genre": song.genre})
 }
 
 // This function executes every time a song is purchased
 // The "song" object contains the following properties: title, artist, genre, duration, price
 // e.g. calling song.title will return the song's title
 function songPurchased(song) {
+	mixpanel.track("Song Purchase", {"Title": song.title, "Artist": song.artist, "Genre": song.genre})
 	
 }
 
 // This function executes when a user upgrades from a Free plan to a Premium plan
 function planUpgraded() {
+	mixpanel.track("Plan Upgraded", {"Plan": user.plan})
 	
 }
 
